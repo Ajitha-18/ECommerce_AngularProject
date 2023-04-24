1e6cd534d69dc4febd145fa2043f4d555466f5a3
Ajitha Manikandan <Ajitha.M@datayaan.com>
2023-04-24 19:04:58 +0530

    1st Commit - Basic creation and Firebase setup

--- ECommerce/src/app/app.module.ts
+++ ECommerce/src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule} from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule
+   
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
---
+++ ECommerce/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyBovwunbmY_HZDs6oH5IbExB0HZIj-Y8n0",
+    authDomain: "ecommerce-akm.firebaseapp.com",
+    projectId: "ecommerce-akm",
+    storageBucket: "ecommerce-akm.appspot.com",
+    messagingSenderId: "625581001968",
+    appId: "1:625581001968:web:10c674e6da8f0004bac635",
+    measurementId: "G-W2WBRDMNCK"
+  },
+  production:false
+};
+
